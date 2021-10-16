033d82018fb399e4fdfac115f680e1034b859917
Mouhyidine OUEDRAOGO <56672820+mouhyib27@users.noreply.github.com>
2021-10-16 10:36:30 +0000

    Setting up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'shopping-cart',
+    component: ShoppingCartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
